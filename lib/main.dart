9285069ee463fbf3a7a6014671e7412c053159da
bytefi.sh <up2279917@myport.ac.uk>
2025-10-09 20:45:26 +0100

    Week 1 - Change style of text - Remove counter

--- lib/main.dart
+++ lib/main.dart
@@ -42,17 +42,19 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const Text(
+              'Welcome to my shop!',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.indigo,
+              ),
             ),
           ],
         ),
